9deb5908b969c7feff21bd13967780d24c11e50b
szluis <szluis@outlook.com>
2019-02-18 16:01:19 -0400

    images dir added and images server url return added

--- handlePictureUpload.php
+++ handlePictureUpload.php
@@ -1,15 +1,16 @@
 <?php
 	// requires php5
-	// define('UPLOAD_DIR', 'images/');
+	define('UPLOAD_DIR', 'images/');
 	$img = $_POST['picture'];
 	
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
 	$data = base64_decode($img);
-	$file = uniqid() . '.png';
+	$file = UPLOAD_DIR . uniqid() . '.png';
 	$success = file_put_contents($file, $data);
-	print $success ? $file : 'Unable to save the file.';
-?>
-<!-- $allowedTypes = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+	print $success ?  $_SERVER['SERVER_NAME'] . $_SERVER['host'] . "/" . $file : 'Unable to save the file.';
+
+	/* $allowedTypes = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
 $detectedType = exif_imagetype($_FILES['fupload']['tmp_name']);
-$error = !in_array($detectedType, $allowedTypes); -->
+$error = !in_array($detectedType, $allowedTypes); */
+?>
