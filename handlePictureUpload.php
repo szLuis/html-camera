cd7ab6de3b1113eb0243591277f23f0d3b5b8926
szluis <szluis@outlook.com>
2019-02-18 15:52:47 -0400

    images dir removed

--- handlePictureUpload.php
+++ handlePictureUpload.php
@@ -1,12 +1,12 @@
 <?php
 	// requires php5
-	define('UPLOAD_DIR', 'images/');
+	// define('UPLOAD_DIR', 'images/');
 	$img = $_POST['picture'];
 	
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
 	$data = base64_decode($img);
-	$file = UPLOAD_DIR . uniqid() . '.png';
+	$file = uniqid() . '.png';
 	$success = file_put_contents($file, $data);
 	print $success ? $file : 'Unable to save the file.';
 ?>
